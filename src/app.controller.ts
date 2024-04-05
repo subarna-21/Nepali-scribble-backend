2278fb8e3d85f08b6df459e7cda6e3da6258307a
Subarna Raj Upreti <usubarna21@tbc.edu.np>
2024-04-05 23:55:20 +0545

    healthchech

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/decorators/route-type.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @HttpCode(200)
   @Get('health')
   getHello() {}
